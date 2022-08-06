3fe175410f20911362f8dd2b35ba50ed2be32db6
Christian Skorobogatow <christiansko@icloud.com>
2022-08-06 18:55:31 +0200

    Initial Commit

---
+++ Generics/GenericsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenericsApp.swift
+//  Generics
+//
+//  Created by Christian Skorobogatow on 6/8/22.
+//
+
+import SwiftUI
+
+@main
+struct GenericsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
