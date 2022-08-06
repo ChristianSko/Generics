47123300ebcfcd846260c55ab1d42acf9c52fc98
Christian Skorobogatow <christiansko@icloud.com>
2022-08-06 19:20:05 +0200

    feat(): example generics

--- Generics/ContentView.swift
+++ Generics/ContentView.swift
@@ -7,10 +7,71 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct StringModel {
+    let info: String?
+    func removeInfo() -> StringModel {
+        StringModel(info: nil)
+    }
+}
+
+struct BoolModel {
+    let info: Bool?
+    func removeInfo() -> BoolModel {
+        BoolModel(info: nil)
+    }
+}
+
+struct GenericModel<T> {
+    let info: T?
+    func removeInfo() -> GenericModel {
+        GenericModel(info: nil)
+    }
+}
+
+class GenericViewModel: ObservableObject {
+    @Published var stringModel = StringModel(info: "Hello World")
+    @Published var boolModel = BoolModel(info: true)
+    
+    @Published var genericStringModel = GenericModel(info: "Hello World")
+    @Published var genericBoolModel = GenericModel(info: true)
+    
+    func removeData() {
+        stringModel = stringModel.removeInfo()
+        boolModel = boolModel.removeInfo()
+        genericStringModel = genericStringModel.removeInfo()
+        genericBoolModel = genericBoolModel.removeInfo()
+    }
+    
+}
+
+struct GenericView<T: View>: View {
+    let content: T
+    let title: String
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text(title)
+            content
+        }
+    }
+}
+
+struct ContentView: View {
+    
+    @StateObject var vm = GenericViewModel()
+    
+    var body: some View {
+        VStack {
+            GenericView(content: Text("custom content"), title: "New View")
+            
+            Text(vm.stringModel.info ?? "no data")
+            Text(vm.boolModel.info?.description ?? "no data")
+            Text(vm.genericStringModel.info ?? "no data")
+            Text(vm.genericBoolModel.info?.description ?? "no data")
+        }
+        .onTapGesture {
+            vm.removeData()
+        }
     }
 }
 
